501d8241d9f8cc9e1faa269c2de4f8f6d94c25ba
RobinSlimani <RobinSlimani>
2024-05-23 17:12:13 +0200

    ajout du fonctionnement formulaire (recocher les cases comme elles étaient) et compteur de cases cochées

--- morpion.js
+++ morpion.js
@@ -2,10 +2,24 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
+let checkboxStates = {};
+let compteur = 0;
+
 app.set('view engine', 'ejs');
+
+app.use(express.urlencoded({ extended: true }));
+
+app.post('/submit', (req, res) => {
+    for (let i = 1; i <= 9; i++) {
+        if (req.body[i] === 'on'){
+            compteur +=1;
+            console.log(compteur);
+        }
+    }
+});
+
 app.get('/', (req, res) => {
-    res.render('morpion');
-    console.log(query);
+    res.render('morpion', { checkboxStates });
 });
 
 app.listen(port, () => {
