6bb04bdde8ee44ac68153e13d1a5d155c2b532c9
RobinSlimani <RobinSlimani>
2024-05-27 08:53:54 +0200

    ajout nouvelle page victoire et hidden input pour conserver les cases cochées

--- morpion.js
+++ morpion.js
@@ -4,7 +4,6 @@ const port = 3000;
 
 let checkboxStates = {};
 
-
 app.set('view engine', 'ejs');
 
 app.use(express.urlencoded({ extended: true }));
@@ -12,46 +11,42 @@ app.use(express.urlencoded({ extended: true }));
 app.post('/submit', (req, res) => {
     let compteur = 0;
     let coche = {};
+    checkboxStates = {}; 
+    
     for (let i = 1; i <= 9; i++) {
-        if (req.body[i] === 'on'){
-            compteur +=1;
+        if (req.body[i] === 'on') {
+            compteur += 1;
             coche[i] = true;
-            console.log(compteur);
-            console.log(coche);
+            checkboxStates[i] = 'on';
+        }
+    }
+    console.log(compteur);
+    console.log(coche);
+
+    if (compteur === 3) {
+        const winningCombinations = [
+            [1, 2, 3], [4, 5, 6], [7, 8, 9],
+            [1, 4, 7], [2, 5, 8], [3, 6, 9],
+            [1, 5, 9], [3, 5, 7]
+        ];
+
+        for (const combination of winningCombinations) {
+            const [a, b, c] = combination;
+            if (coche[a] && coche[b] && coche[c]) {
+                return res.redirect('/win');
+            }
         }
     }
 
-    if (compteur === 3){
-        if (coche[1] && coche[2] && coche[3]) {
-            console.log("GAGNE")
-        }
-        if (coche[4] && coche[5] && coche[6]) {
-            console.log("GAGNE")
-        }
-        if (coche[7] && coche[8] && coche[9]) {
-            console.log("GAGNE")
-        }
-        if (coche[1] && coche[4] && coche[7]) {
-            console.log("GAGNE")
-        }
-        if (coche[2] && coche[5] && coche[8]) {
-            console.log("GAGNE")
-        }
-        if (coche[3] && coche[6] && coche[9]) {
-            console.log("GAGNE")
-        }
-        if (coche[1] && coche[5] && coche[9]) {
-            console.log("GAGNE")
-        }
-        if (coche[3] && coche[5] && coche[7]) {
-            console.log("GAGNE")
-        }
-    }
-    
+    res.render('morpion1', { checkboxStates, compteur });
+});
+
+app.get('/win', (req, res) => {
+    res.render('win');
 });
 
 app.get('/', (req, res) => {
-    res.render('morpion', { checkboxStates });
+    res.render('morpion1', { checkboxStates, compteur:0 });
 });
 
 app.listen(port, () => {
